b1d79d34407c89f1a1160009e7492b903484455b
Yassh Singh <90470603+iyassh@users.noreply.github.com>
2021-09-29 02:28:04 +0530

    Update PoundsToKilograms.java

--- PoundsToKilograms.java
+++ PoundsToKilograms.java
@@ -1,20 +1,26 @@
-package Assigenment;
+/*
+   This program converts Pounds to Kilograms.
+     By Group A Members: Behlah
+                         Anjali
+                         Yassh
+
+*/
 import java.util.Scanner;
 
 public class PoundsToKilograms {
-    static final double PND2KG = 0.453592;
+    static final double PND2KG = 0.453592;// import Scanner class
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);//attach to System.in
 
-        double inputPounds = 0.0;
-        double resultKilograms = 0.0;
+        double inputPounds = 0.0; //hold pounds value provided by the user
+        double resultKilogram= 0.0; //hold calculated kilogram value
 
         System.out.println("Pounds to Kilograms Converter");
         System.out.println(" How many pounds?");
-        inputPounds = scan.nextDouble();
+        inputPounds = scan.nextDouble();//get gallons from user
+        resultKilogram=inputPounds * PND2KG;//conversion formula
 
-        double resultKilograms = inputPounds * PND2KG;
-        System.out.println("Converted: " + inputPounds + " pounds" + " = " +resultKilograms+ " kilograms");
+        System.out.println("Converted: " + inputPounds + " pounds" + " = " +resultKilogram+ " kilograms");//display result
     }
 }
