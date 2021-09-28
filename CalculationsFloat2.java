c792e2323ebea8258dc6a76d4dc1a2fb239b1c2e
Yassh Singh <90470603+iyassh@users.noreply.github.com>
2021-09-29 02:25:00 +0530

    Update CalculationsFloat2.java

--- CalculationsFloat2.java
+++ CalculationsFloat2.java
@@ -1,37 +1,44 @@
- import java.util.Scanner;
+/*
+    This program performs calculations on the two numbers given by the user.
+     By Group A Members: Behlah
+                         Anjali
+                         Yassh
+
+*/
+    import java.util.Scanner; // import Scanner class
     public class CalculationsFloat {
 
         public static void main(String[] args)
         {
             Scanner scan = new Scanner(System.in);
-            float inputNum1 ;
-            float inputNum2 ;
+            float inputNum1 ;// variable declaration
+            float inputNum2 ;// variable declaration
 
             System.out.println("Please enter two values:");
             System.out.print("Value A:");
-            inputNum1 = scan.nextInt();
+            inputNum1 = scan.nextInt();// taking user`s 1st number
             System.out.print("Value B:");
-            inputNum2 = scan.nextInt();
+            inputNum2 = scan.nextInt();// taking user`s 1st number
 
-            float resADD ;
-            float resSUB ;
-            float resMUL ;
-            float resDIV ;
-            float resREM ;
+            float resADD ;// variable declaration
+            float resSUB ;// variable declaration
+            float resMUL ;// variable declaration
+            float resDIV ;// variable declaration
+            float resREM ;// variable declaration
 
             System.out.println("The calculations for "+inputNum1+" and "+inputNum2 + " are:" );
 
-            resADD = inputNum1 + inputNum2;
-            resSUB = inputNum1 - inputNum2;
-            resMUL = inputNum1 * inputNum2;
-            resDIV = inputNum1 / inputNum2;
-            resREM = inputNum1 % inputNum2;
+            resADD = inputNum1 + inputNum2;// performing calculations
+            resSUB = inputNum1 - inputNum2;// performing calculations
+            resMUL = inputNum1 * inputNum2;// performing calculations
+            resDIV = inputNum1 / inputNum2;// performing calculations
+            resREM = inputNum1 % inputNum2;// performing calculations
 
-            System.out.println("\t"+inputNum1 +" + "+inputNum2+" = "+ resADD);
-            System.out.println("\t"+inputNum1 +" - "+inputNum2+" = "+ resSUB);
-            System.out.println("\t"+inputNum1 +" * "+inputNum2+" = "+ resMUL);
-            System.out.println("\t"+inputNum1 +" / "+inputNum2+" = "+ resDIV);
-            System.out.println("\t"+inputNum1 +" % "+inputNum2+" = "+ resREM);
+            System.out.println("\t"+inputNum1 +" + "+inputNum2+" = "+ resADD);//output statement
+            System.out.println("\t"+inputNum1 +" - "+inputNum2+" = "+ resSUB);//output statement
+            System.out.println("\t"+inputNum1 +" * "+inputNum2+" = "+ resMUL);//output statement
+            System.out.println("\t"+inputNum1 +" / "+inputNum2+" = "+ resDIV);//output statement
+            System.out.println("\t"+inputNum1 +" % "+inputNum2+" = "+ resREM);//output statement
 
         }
 
