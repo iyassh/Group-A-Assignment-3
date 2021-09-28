0e21dd532a9a546ba2922212b7b3030f803994e8
Yassh Singh <90470603+iyassh@users.noreply.github.com>
2021-09-29 02:27:15 +0530

    Update MilesToKilometres.java

--- MilesToKilometres.java
+++ MilesToKilometres.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+/*
+    This program converts Miles to Kilometers.
+     By Group A Members: Behlah
+                         Anjali
+                         Yassh
 
-public classMilestokilometres{
-    static final double ML2KM = 1.60935;
+*/
+import java.util.Scanner;// import Scanner class
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double inputMiles = 0.0;
-        double resultKilometres = 0.0;
+public class Milestokilometres{
+static final double ML2KM = 1.60935;//conversion factor
+
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); //attach to System.in
+        double inputMiles = 0.0; //hold miles value provided by the user
+        double resultKilometres = 0.0; //hold calculated kilometer value
 
         System.out.println("Miles to Kilometres Converter");
         System.out.print("How many miles?");
-        inputMiles = scan.nextDouble();
+        inputMiles = scan.nextDouble(); //get gallons from user
 
-        resultKilometres = inputMiles * ML2KM;
+        resultKilometres = inputMiles * ML2KM;//conversion formula
         System.out.print ("Converted:" + inputMiles +"miles "+ "=");
-        System.out.println(resultKilometres + " kilometres");
+        System.out.println(resultKilometres + " kilometres");//display result
         scan.close();
-    }
+        }
 }
